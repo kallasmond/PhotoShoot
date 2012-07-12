a95c5a3cf46a242ac815454305846e5c102e08f7
Aleksandra Bondzic <abondzic@thoughtworks.com>
2012-07-12 00:27:01 -0400

    Created Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoShoot::Application.config.secret_token = 'c77c9465bc502fe83ee18f7b30897e511ebf7cf5d2d9f4cea9c9072a81807b94728b81eb5f5da8f6191a022dff66a279131524b06d1c31c20b7dace8cdd937f9'
